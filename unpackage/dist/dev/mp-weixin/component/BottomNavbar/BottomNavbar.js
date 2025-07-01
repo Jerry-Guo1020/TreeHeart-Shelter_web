92017bfcdd47b56b536ae4451ad28afb9bc057e0
Jerry-Guo1020 <2593517388@qq.com>
2025-07-01 09:03:44 +0800

    feat: 优化了项目结构

--- unpackage/dist/dev/mp-weixin/component/BottomNavbar/BottomNavbar.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const BottomNavbar = require("../../BottomNavbar.js");
-wx.createPage(BottomNavbar.MiniProgramPage);
-//# sourceMappingURL=../../../.sourcemap/mp-weixin/component/BottomNavbar/BottomNavbar.js.map
--- unpackage/dist/dev/mp-weixin/component/BottomNavbar/BottomNavbar2.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const BottomNavbar = require("../../BottomNavbar.js");
-wx.createPage(BottomNavbar.MiniProgramPage);
-//# sourceMappingURL=../../../.sourcemap/mp-weixin/component/BottomNavbar/BottomNavbar2.js.map
